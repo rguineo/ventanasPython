c1cff472004b866a939e941f2ae19c43936dfbf0
Roberto Guineo <rguineoa@gmail.com>
2024-08-31 17:32:13 -0400

    agrega insertar registro a la DB

--- windows_menu.py
+++ windows_menu.py
@@ -1,6 +1,6 @@
 import tkinter as tk
-from tkinter import ttk, Tk, Label, Button, messagebox, Menu
-from database import selectDB
+from tkinter import ttk, Tk, Label, Button, messagebox, Menu, Entry
+from database import selectDB, insertDB
 
 # Variables
 rows = []
@@ -28,6 +28,19 @@ def backToMenu():
     
     principal.deiconify() #mostrar ventana principal
 
+def funcBtnInsert():
+    username = input1.get() 
+    password = input2.get()
+    firstName = input3.get()
+    lastName = input4.get()
+
+    insertDB(username, password, firstName, lastName)
+
+    input1.delete(0, 'end')
+    input2.delete(0, 'end')
+    input3.delete(0, 'end')
+    input4.delete(0, 'end')
+
 def findUsers():
     rows = selectDB()
 
@@ -51,8 +64,40 @@ principal.geometry("600x400")
 # Creacion de ventana Nuevo documento
 nuevo = Tk()
 nuevo.title("Nuevo documento")
-nuevo.geometry("00x400")
+nuevo.geometry("600x400")
 btnReturn = Button(nuevo, text="<< Regresar Menu Principal", command=backToMenu).pack()
+
+# Los Labels son textos que hacen referencia una Caja de Texto
+label1 = Label(nuevo, text="Nombre Usuario")
+label1.place(relx=0.01, rely=0.05) 
+
+# Place es la ubicacion en la ventana. relx, rely son posiciones relativas, es decir, posicion establecida a traves de un porcentaje.
+
+input1 = Entry(nuevo) #Entry es un cuadro de texto o caja de texto
+input1.place(relx=0.2, rely=0.05) 
+
+label2 = Label(nuevo, text="Password")
+label2.place(relx=0.01, rely=0.12)
+
+input2 = Entry(nuevo)
+input2.place(relx=0.2, rely=0.12)
+
+label3 = Label(nuevo, text="Nombre")
+label3.place(relx=0.01, rely=0.19)
+
+input3 = Entry(nuevo)
+input3.place(relx=0.2, rely=0.19)
+
+label4 = Label(nuevo, text="Apellido")
+label4.place(relx=0.01, rely=0.26)
+
+input4 = Entry(nuevo)
+input4.place(relx=0.2, rely=0.26)
+
+btn = Button(nuevo, text="Guardar", command=funcBtnInsert) #Button es un boton para realizar acciones, el command hace un llamado a una funcion
+
+btn.place(relx=0.55, rely=0.05, relwidth=0.15, relheight=0.12)
+
 nuevo.withdraw() # ocultar ventana
 
 # Creacion de ventana Guardar
